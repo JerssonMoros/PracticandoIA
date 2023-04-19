13626da0d846e0c398ab47f15db6c50726c5cae2
Jersson Moros <jersson0423@gmail.com>
2023-04-19 17:18:37 -0500

    starting

---
+++ ejercicio.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+print("Hola Mundo") 
